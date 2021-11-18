5ed6342fc3edfcc59a6e3167befb100929d168cb
Aleksandr Tarasov <alan.trsv@gmail.com>
2021-11-18 10:14:00 +0500

    Section 3. Basket.API

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,42 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            _redisCache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+
+            if (String.IsNullOrEmpty(basket))
+                return null;            
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+        
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            var basetJsonStr = JsonConvert.SerializeObject(basket);
+            await _redisCache.SetStringAsync(basket.UserName, basetJsonStr);
+            
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
